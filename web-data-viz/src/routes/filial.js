58294e9e18f5bfe47ec228e492fe9e1753e548ec
paulo15002 <paulo.silva@sptech.school>
2026-06-06 00:05:29 -0300

    Adicionando funcão de adicionar um responsavel/gerente ja existente a uma filial recem cadastrada, tela gerenciamento Filiais

--- web-data-viz/src/routes/filial.js
+++ web-data-viz/src/routes/filial.js
@@ -15,4 +15,12 @@ router.get("/listarFiliais/:idMatriz", function(req, res) {
     gerenciamentoFilialController.listarFiliais(req, res)
 })
 
+router.get("/listarResponsaveis/:idMatriz", function (req, res) {
+    gerenciamentoFilialController.listarResponsaveis(req, res)
+})
+
+router.post("/vincularResponsavel", function (req, res) {
+    gerenciamentoFilialController.vincularResponsavel(req, res)
+})
+
 module.exports = router;
