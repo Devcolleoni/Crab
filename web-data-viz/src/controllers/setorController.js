5442c5532466dd2a0481f2f1aa1fc8354d5c249e
Gustavo Coleoni <gustavo.ccarvalho3@senacsp.edu.br>
2026-06-06 15:54:52 -0300

    ajustes na tela de setores

--- web-data-viz/src/controllers/setorController.js
+++ web-data-viz/src/controllers/setorController.js
@@ -1,69 +1,69 @@
-var setorModel = require("../models/setorModel");
+    var setorModel = require("../models/setorModel");
 
-function cadastrar(req, res) {
+    function cadastrar(req, res) {
+        let nomeSetor = req.body.nomeSetorServer;
+        let vaoInicial = Number(req.body.vaoInicialServer);
+        let vaoFinal = Number(req.body.vaoFinalServer);
+        
+    
+        let idFilial = req.body.idFilialServer || 1;
+        let idMatriz = req.body.idMatrizServer || 1;
 
-    let nomeSetor = req.body.nomeSetorServer;
-    let vaoInicial = req.body.vaoInicialServer;
-    let vaoFinal = req.body.vaoFinalServer;
-    let idFilial = req.body.idFilialServer;
-    let idMatriz = req.body.idMatrizServer;
+        if (nomeSetor == undefined || nomeSetor == "") {
+            res.status(400).send("O nome do setor está inválido!");
+        } else if (isNaN(vaoInicial) || isNaN(vaoFinal)) {
+            res.status(400).send("Os vãos inicial e final precisam ser números válidos!");
+        } else {
+        
+            setorModel.cadastrar(nomeSetor, vaoInicial, vaoFinal, idFilial, idMatriz)
+            .then(function(resultado) {
+                res.status(200).json(resultado);
+            })
+            .catch(function(erro) {
+                console.log(erro);
+                res.status(500).json(erro.sqlMessage);
+            });
+        }
+    }
 
-    setorModel.cadastrar(
-        nomeSetor,
-        vaoInicial,
-        vaoFinal, 
-        idFilial,
-        idMatriz
-    )
-    .then(function(resultado) {
-        res.status(200).json(resultado);
-    })
-    .catch(function(erro) {
-        console.log(erro);
-        res.status(500).json(erro.sqlMessage);
-    });
-}
+    function listar(req, res) {
+        setorModel.listar()
+        .then(function(resultado) {
+            res.json(resultado);
+        })
+        .catch(function(erro) {
+            res.status(500).json(erro.sqlMessage);
+        });
+    }
 
-function listar(req, res) {
+    function remover(req, res) {
+        let idSetor = req.params.idSetor;
 
-    setorModel.listar()
-    .then(function(resultado) {
-        res.json(resultado);
-    })
-    .catch(function(erro) {
-        res.status(500).json(erro.sqlMessage);
-    });
-}
+        setorModel.remover(idSetor)
+        .then(function(resultado) {
+            res.json(resultado);
+        })
+        .catch(function(erro) {
+            res.status(500).json(erro.sqlMessage);
+        });
+    }
 
-function remover(req, res) {
+    function editar(req, res) {
+        let idSetor = req.params.idSetor;
+        let nome = req.body.nomeServer;
 
-    var idSetor = req.params.idSetor;
-
-    setorModel.remover(idSetor)
-    .then(function(resultado) {
-        res.json(resultado);
-    })
-    .catch(function(erro) {
-        res.status(500).json(erro.sqlMessage);
-    });
-}
-
-function editar(req, res) {
-    var idSetor = req.params.idSetor;
-    var nome = req.body.nomeServer;
-
-    setorModel.editar(idSetor, nome)
+        setorModel.editar(idSetor, nome)
         .then(function (resultado) {
             res.json(resultado);
         })
         .catch(function (erro) {
             res.status(500).json(erro.sqlMessage);
         });
-}
+    }
 
-module.exports = {
-    cadastrar,
-    listar,
-    remover,
-    editar
-};
+    module.exports = {
+        cadastrar,
+        listar,
+        remover,
+        editar
+    };
--- web-data-viz/src/models/setorModel.js
+++ web-data-viz/src/models/setorModel.js
@@ -1,115 +1,84 @@
-var database = require("../database/config");
+    var database = require("../database/config");
 
-var database = require("../database/config");
+    function cadastrar(nomeSetor, vaoInicial, vaoFinal, idFilial, idMatriz) {
+     
+        let instrucaoSqlSetor = `
+            INSERT INTO setor(nome) VALUES ('${nomeSetor}');
+        `;
+        
+        console.log("Executando inserção do Setor");
+        
+ 
+        return database.executar(instrucaoSqlSetor).then(function (resultadoSetor) {
+            let idSetorGerado = resultadoSetor.insertId;
 
-function cadastrar(
-    nomeSetor,
-    vaoInicial,
-    vaoFinal,
-    idFilial,
-    idMatriz
-) {
-    var sqlFilial = idFilial ? idFilial : 'NULL';
-    var sqlMatriz = idMatriz ? idMatriz : 'NULL';
+        
+            let instrucaoSqlVao = `INSERT INTO vao (numero, id_filial, id_matriz, id_setor) VALUES `;
 
-    
-    var sqlValidar = `
-        SELECT COUNT(*) AS total 
-        FROM vao 
-        WHERE id_matriz = ${sqlMatriz} 
-          AND id_filial = ${sqlFilial} 
-          AND numero BETWEEN ${vaoInicial} AND ${vaoFinal};
-    `;
-
-    return database.executar(sqlValidar)
-        .then(function (resultadoValidacao) {
-            var vaosOcupados = resultadoValidacao[0].total;
-
-            
-            if (vaosOcupados > 0) {
-            
-                throw new Error("Conflito: Um ou mais vãos neste intervalo já estão ocupados por outro setor!");
-            }
-
-            
-            var sqlSetor = `
-                INSERT INTO setor (nome, vao_inicial, vao_final)
-                VALUES ('${nomeSetor}', ${vaoInicial}, ${vaoFinal});
-            `;
-
-            return database.executar(sqlSetor);
-        })
-        .then(function (resultadoSetor) {
-            var idSetor = resultadoSetor.insertId || (resultadoSetor && resultadoSetor.insertId);
-
-            if (!idSetor) {
-                throw new Error("Não foi possível recuperar o ID do setor inserido.");
-            }
-
-            
-            let valores = [];
             for (let i = vaoInicial; i <= vaoFinal; i++) {
-                valores.push(
-                    `(${i}, ${sqlFilial}, ${sqlMatriz}, ${idSetor})`
-                );
+                instrucaoSqlVao += `(${i}, ${idFilial}, ${idMatriz}, ${idSetorGerado})`;
+
+               
+                if (i == vaoFinal) {
+                    instrucaoSqlVao += `;`;
+                } else {
+                    instrucaoSqlVao += `, `;
+                }
             }
 
-            var sqlVaos = `
-                INSERT INTO vao (numero, id_filial, id_matriz, id_setor)
-                VALUES ${valores.join(",")};
-            `;
-
-            return database.executar(sqlVaos);
+            console.log("Executando inserção dos vãos em lote:\n" + instrucaoSqlVao);
+            
+       
+            return database.executar(instrucaoSqlVao).then(function () {
+                return resultadoSetor;
+            });
         });
-}
-
-
-
-function listar() {
-
-    var instrucaoSql = `
-    SELECT * FROM setor;
-    `;
-
-    return database.executar(instrucaoSql);
-}
-
-    function remover(idSetor) {
-    if (idSetor == undefined || idSetor == null || idSetor == "undefined") {
-        return Promise.reject("Erro no Model: O idSetor enviado é inválido ou undefined!");
     }
 
-    var sqlDeletarVaos = `
-        DELETE FROM vao
-        WHERE id_setor = ${idSetor};
-    `;
+    function listar() {
+        let instrucaoSql = `
+            SELECT 
+                s.id_setor, 
+                s.nome, 
+                MIN(v.numero) AS vao_inicial,
+                MAX(v.numero) AS vao_final
+            FROM setor s
+            LEFT JOIN vao v ON s.id_setor = v.id_setor
+            GROUP BY s.id_setor, s.nome;
+        `;
+        console.log("Executando a instrução SQL de listagem (Intervalos): \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
+    }
 
-    return database.executar(sqlDeletarVaos)
-        .then(function () {
-            var sqlDeletarSetor = `
-                DELETE FROM setor
-                WHERE id_setor = ${idSetor};
+    function remover(idSetor) {
+        
+        let instrucaoVaos = `
+            DELETE FROM vao WHERE id_setor = ${idSetor};
+        `;
+        
+        console.log("Removendo vãos do setor: " + idSetor);
+        
+        return database.executar(instrucaoVaos).then(function () {
+            let instrucaoSetor = `
+                DELETE FROM setor WHERE id_setor = ${idSetor};
             `;
-            return database.executar(sqlDeletarSetor);
+            console.log("Removendo o setor de fato: " + idSetor);
+            return database.executar(instrucaoSetor);
         });
-}
+    }
 
+    function editar(idSetor, nome) {
+        let instrucaoSql = `
+            UPDATE setor
+            SET nome = '${nome}'
+            WHERE id_setor = ${idSetor};
+        `;
+        return database.executar(instrucaoSql);
+    }
 
-
-function editar(idSetor, nome) {
-
-    var instrucaoSql = `
-        UPDATE setor
-        SET nome = '${nome}'
-        WHERE id_setor = ${idSetor};
-    `;
-
-    return database.executar(instrucaoSql);
-}
-
-module.exports = {
-    cadastrar,
-    listar,
-    remover,
-    editar
-};
+    module.exports = {
+        cadastrar,
+        listar,
+        remover,
+        editar
+    };
